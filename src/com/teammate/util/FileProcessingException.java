1e61c98a5839360d9b230427bd8e5a0b9fd14360
krisanthisegar <krishsegar.12@gmail.com>
2025-12-05 21:03:02 +0530

    fix: changed and fix service classes

--- src/com/teammate/util/FileProcessingException.java
+++ src/com/teammate/util/FileProcessingException.java
@@ -5,7 +5,7 @@ package com.teammate.util;
  *
  * Thrown when file operations fail (file not found, read/write errors, etc.)
  *
- * @author Student Name
+ * @author Krisanthi Segar 2425596
  * @version 1.0
  * @since 2025
  */
