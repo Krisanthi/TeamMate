1af0996247ac3f49a96445d4208dff9cf2332c60
krisanthisegar <krishsegar.12@gmail.com>
2025-12-05 15:21:20 +0530

    feat: add supporting methods for required operations

--- src/com/teammate/util/InvalidInputException.java
+++ src/com/teammate/util/InvalidInputException.java
@@ -1,14 +1,29 @@
 package com.teammate.util;
 
 /**
- * Custom exception for invalid user inputs
+ * InvalidInputException - Custom Exception for Invalid User Input
+ *
+ * Thrown when user input fails validation checks
+ *
+ * @author Student Name
+ * @version 1.0
+ * @since 2025
  */
 public class InvalidInputException extends Exception {
 
+    /**
+     * Constructs exception with detail message
+     * @param message The detail message
+     */
     public InvalidInputException(String message) {
         super(message);
     }
 
+    /**
+     * Constructs exception with message and cause
+     * @param message The detail message
+     * @param cause The underlying exception
+     */
     public InvalidInputException(String message, Throwable cause) {
         super(message, cause);
     }
